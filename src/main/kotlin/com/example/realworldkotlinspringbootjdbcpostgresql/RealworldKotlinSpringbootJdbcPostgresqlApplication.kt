e9e3b056c54033d6a14be0fd2ea73cda92e27589
Msksgm <msksgm3124@gmail.com>
2022-06-17 07:51:48 +0900

    first commit

---
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbcpostgresql/RealworldKotlinSpringbootJdbcPostgresqlApplication.kt
@@ -0,0 +1,11 @@
+package com.example.realworldkotlinspringbootjdbcpostgresql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RealworldKotlinSpringbootJdbcPostgresqlApplication
+
+fun main(args: Array<String>) {
+	runApplication<RealworldKotlinSpringbootJdbcPostgresqlApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbcpostgresql/RealworldKotlinSpringbootJdbcPostgresqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.realworldkotlinspringbootjdbcpostgresql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RealworldKotlinSpringbootJdbcPostgresqlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
